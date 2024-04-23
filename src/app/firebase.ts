1e3d3cc2e5df9d4b6f32d7a6a1a2348056f6ed23
vaxo <vaxojaniashvili186@gmail.com>
2024-04-23 18:22:48 +0400

    updated readme folder

---
+++ src/app/firebase.ts
@@ -0,0 +1,14 @@
+import { initializeApp } from "firebase/app";
+import {getAuth,GoogleAuthProvider} from 'firebase/auth'
+const firebaseConfig = {
+  apiKey: "AIzaSyBxdZALB373aVn9SYaXpDvroJNui5A6heI",
+  authDomain: "first-app-9a2f5.firebaseapp.com",
+  projectId: "first-app-9a2f5",
+  storageBucket: "first-app-9a2f5.appspot.com",
+  messagingSenderId: "517573665442",
+  appId: "1:517573665442:web:710023089fbd4ccb266dfb"
+};
+
+const app = initializeApp(firebaseConfig);
+export const auth = getAuth(app)
+export const googleProvider = new GoogleAuthProvider()
---
+++ src/app/pages/Sign-Up/page.tsx
@@ -0,0 +1,12 @@
+import SignUpInput from '@/app/common/components/SignUpInput'
+import React from 'react'
+
+const SignUp = () => {
+  return (
+    <>
+    <SignUpInput/>
+    </>
+  )
+}
+
+export default SignUp
