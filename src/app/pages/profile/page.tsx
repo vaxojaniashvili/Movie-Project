feda28937f3847460ffc5e513cc16c4ac2ae02d9
vaxo <vaxojaniashvili186@gmail.com>
2024-04-24 13:00:56 +0400

    updated-routerrs

--- src/app/pages/profile/page.tsx
+++ src/app/pages/profile/page.tsx
@@ -1,14 +1,33 @@
 "use client";
 import { auth } from "@/app/firebase";
+import { useRouter } from "next/navigation";
 import React from "react";
 import { useAuthState } from "react-firebase-hooks/auth";
 
 const page = () => {
+  const router = useRouter();
   const [user, setUser] = useAuthState(auth);
+  console.log(user);
+  const handleLogout = async () => {
+    try {
+      await auth.signOut();
+      router.push("/");
+    } catch (error) {
+      console.log("error", error);
+    }
+  };
   return (
     <div className="flex justify-center items-center">
       <div className="grid gap-5">
-        <h1 className="flex justify-center">Profile</h1>
+        <div className="flex gap-5 justify-center">
+          <h1 className="flex justify-center">Profile</h1>
+          <button
+            onClick={handleLogout}
+            className="bg-black rounded text-white px-5"
+          >
+            logOut
+          </button>
+        </div>
         <div>
           {user ? (
             <div>
